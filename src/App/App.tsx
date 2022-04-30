8442eca9bff61c428a369fd96ae3b8b2111ed922
Luke McCann <Developer-lukemccann@outlook.com>
2022-04-30 12:26:53 +0100

    Update App.tsx

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 interface AppProps {}
 
-export const App: React.FC = ({}: AppProps) => (
+export const App: React.FC<AppProps> = ({}) => (
   <div className="App">
     <h1>Welcome to Snowpack!</h1>
   </div>
